376d18b5ca435410235bbf4cd43851ef5386c28e
liam1202 <Liam.Knipper1@marist.edu>
2026-03-26 18:23:43 -0400

    copied over the Tyler's pom dependencies and module imports onto my branch

--- System Watch/src/main/java/module-info.java
+++ System Watch/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.systemwatch {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.github.oshi;
     requires java.sql;
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires java.desktop;
 
     exports com.systemwatch;
     opens com.systemwatch to javafx.fxml;
